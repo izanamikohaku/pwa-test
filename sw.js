c01570a48324f181847be62374b62e1723643180
tienvt <tienvt@rikkeisoft.com>
2019-06-14 17:13:09 +0700

    1

---
+++ sw.js
@@ -0,0 +1,43 @@
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js', 'custom-sw.js')
+
+// --------------------------------------------------
+// Configure
+// --------------------------------------------------
+
+// Set workbox config
+workbox.setConfig({
+  "debug": true
+})
+
+// Start controlling any existing clients as soon as it activates
+workbox.core.clientsClaim()
+
+// Skip over the SW waiting lifecycle stage
+workbox.core.skipWaiting()
+
+workbox.precaching.cleanupOutdatedCaches()
+
+// --------------------------------------------------
+// Precaches
+// --------------------------------------------------
+
+// Precache assets
+
+// -- Start of cachingExtensions --
+workbox.routing.registerRoute(
+  /.*\.(mp4|webm)/,
+  workbox.strategies.cacheFirst({
+    plugins: [
+      new workbox.rangeRequests.Plugin(),
+    ],
+  }),
+  'GET'
+);// -- End of cachingExtensions --
+
+// --------------------------------------------------
+// Runtime Caching
+// --------------------------------------------------
+
+// Register route handlers for runtimeCaching
+workbox.routing.registerRoute(new RegExp('/pwa/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/pwa/'), new workbox.strategies.NetworkFirst ({}), 'GET')
